d496a19171944c9e0299653c50d579f26d27b2c4
Benjamin Williams <deanpbr@gmail.com>
2021-12-10 22:27:16 -0500

    Day 6 done

---
+++ src/main/java/advent_of_code/day_6/Fish.java
@@ -0,0 +1,29 @@
+package advent_of_code.day_6;
+
+public class Fish {
+    private int daysToSpawn;
+
+    public Fish(int daysToSpawn) {
+
+        this.daysToSpawn = daysToSpawn;
+    }
+
+    public int getDaysToSpawn() {
+        return daysToSpawn;
+    }
+
+    public void ageFish(){
+        daysToSpawn--;
+    }
+
+    public void resetSpawnCounter(){
+        daysToSpawn = 6;
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "daysToSpawn =" + daysToSpawn +
+                '}';
+    }
+}
